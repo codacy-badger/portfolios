da40d12c5c06371f4cbc6a08cf2ad9bd1bd03d45
Minh-Hieu Nguyen <richardn1999@openlab09.highline.edu>
2020-02-20 11:10:31 -0800

    :wrench: Specify title bar to drag

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,7 +1,67 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react'
 import styled from 'styled-components'
+import Draggable from 'react-draggable'
 
 import { Heading } from '@components'
+
+const StyledBtn = styled.button`
+  border-radius: 50%;
+  border: none;
+
+  width: 14px;
+  height: 14px;
+
+  padding: 0;
+  margin-right: 0.5rem;
+`
+
+const StyledBtnRed = styled(StyledBtn)`
+  background: rgba(255, 59, 48, 1);
+`
+
+const StyledBtnYellow = styled(StyledBtn)`
+  background: rgba(255, 204, 0, 1);
+`
+
+const StyledBtnGreen = styled(StyledBtn)`
+  background: rgba(40, 205, 65);
+`
+
+const StyledBtnContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: absolute;
+`
+
+const StyledTitle = styled.h1`
+  margin: 0 auto;
+
+  color: white;
+  line-height: 1;
+  height: 14px;
+
+  font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+`
+
+const StyledHeading = styled.div`
+  display: flex;
+
+  position: relative;
+
+  margin: 0;
+  padding: 0.5rem;
+
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background: black;
+
+  width: auto;
+`
 
 const StyledConsole = styled.p`
   padding: 0.5rem;
@@ -39,8 +99,22 @@ const Terminal: React.FC = () => {
   const draggableRef = useRef<HTMLDivElement>(null)
   const dragging = useRef<boolean>(false)
 
-  const onMouseDown = useCallback(() => {
-    if (draggableRef.current) {
+  const matchSelector = (
+    e: EventTarget | null,
+    selector: string,
+    node: (Node & ParentNode) | null
+  ): boolean => {
+    if (node !== null) {
+      return !!node.querySelector(selector)
+    }
+    return false
+  }
+
+  const onMouseDown = useCallback((e: MouseEvent) => {
+    if (
+      draggableRef.current &&
+      matchSelector(e.target, '#handle', draggableRef.current.parentNode)
+    ) {
       dragging.current = true
     }
   }, [])
@@ -71,16 +145,19 @@ const Terminal: React.FC = () => {
   }, [onMouseUp, onMouseDown, onMouseMove])
 
   return (
-    <StyledTerminal
-      ref={draggableRef}
-      style={{
-        left: `${posX.toString().concat('px')}`,
-        top: `${posY.toString().concat('px')}`,
-      }}
-    >
-      <Heading />
-      <StyledConsole>Hello</StyledConsole>
-    </StyledTerminal>
+    <Draggable handle="#handle">
+      <StyledTerminal>
+        <StyledHeading id="handle">
+          <StyledBtnContainer>
+            <StyledBtnRed />
+            <StyledBtnYellow />
+            <StyledBtnGreen />
+          </StyledBtnContainer>
+          <StyledTitle>Children</StyledTitle>
+        </StyledHeading>
+        <StyledConsole>Hello</StyledConsole>
+      </StyledTerminal>
+    </Draggable>
   )
 }
 

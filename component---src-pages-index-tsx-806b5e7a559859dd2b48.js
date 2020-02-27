54bbdd7d677560944caf901bf0d836ba22e54941
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 22:40:16 +0000

    Deploy richardnguyen99/portfolios to github.com/richardnguyen99/portfolios.git:gh-pages

--- component---src-pages-index-tsx-806b5e7a559859dd2b48.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{QeBL:function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),a=n.n(l),c=n("Nhdc");t.default=function(){return a.a.createElement(c.a,null,a.a.createElement(c.b,{title:"Home"}),a.a.createElement("h1",null,"Hi people"),a.a.createElement(c.c,null),a.a.createElement("p",null,"Welcome to your new Gatsby site."),a.a.createElement("p",null,"Now go build something great."))}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-806b5e7a559859dd2b48.js.map
